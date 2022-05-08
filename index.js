7034b14be8de0027f8918fb74956fb3aa797932e
array-in-a-matrix <nova.neutrino@protonmail.com>
2022-05-08 00:38:02 -0400

    better file handling

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ let redirectURL = 'https://arrayinamatrix.xyz/res/site/images/trollface.gif'
 if (process.argv[2] != undefined) {
     redirectURL = process.argv[2]
 }
-
+let logFile = 'ip-addresses.log'
 let httpPort = 3030
+
 console.log(`Redirect: ${redirectURL}`);
 console.log(`Port: ${httpPort}`)
+console.log(`Log file location: ${logFile}`)
 console.log("########## IP LOGGER STARTED ##########");
 
 const app = connect()
@@ -22,14 +24,24 @@ const app = connect()
 
         let time = new Date();
         let ip = req.clientIp;
-        let output = time.getFullYear() + "-" + ("0" + (time.getMonth() + 1)).slice(-2) + "-" + ("0" + time.getDate()).slice(-2) + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + ">> " + ip;
+        let output = time.getFullYear() + "-" + ("0" + (time.getMonth() + 1)).slice(-2) + "-" + ("0" + time.getDate()).slice(-2) + " " + time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + " >> " + ip.slice(7);
 
         console.log(output);
-        file.appendFile("../../res/downloads/ip-addresses.log", output + '\n', (e) => {
-            if (e) {
-                console.log(e);
-            };
-        });
+        try {
+            file.accessSync(logFile, file.constants.R_OK | file.constants.W_OK)
+            file.appendFile(logFile, output + '\n', (e) => {
+                if (e != null) {
+                    console.log(e)
+                }
+            });
+
+        } catch (error) {
+            file.writeFile(logFile, output + '\n', (e) => {
+                if (e != null) {
+                    console.log(e)
+                }
+            });
+        }
     });
 
 http.createServer(app).listen(httpPort);
